381d1a76c4da6827bd0ac5547e1fa7ba4b1935e5
Alex Chaplinsky <alchaplinsky@gmail.com>
2026-03-23 12:37:05 +0800

    Dynamic landing rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { connection } from "next/server";
 import { HypeMeterPage } from "@/components/hype-meter-page";
 import { getTopTrendingQueries } from "@/lib/measure-store";
 
 export default async function Home() {
+  await connection();
   const suggestions = await getTopTrendingQueries(10);
   return <HypeMeterPage suggestions={suggestions} />;
 }
